5546c768ba85e9642cb64cd7ea0b75ea6126e2e6
saikumar457L <saikumarlovely5@gmail.com>
2020-06-16 15:50:36 +0530

    fixed social login

--- check/urls.py
+++ check/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/",include("accounts.urls")),
-    path("socil-auth/",include("social_django.urls",namespace="social")),
+    path("social-auth/",include("social_django.urls",namespace="social")),
 ]
 
 if settings.DEBUG:
